bb968819f786813b303ea807b5509cae67a7b47a
P <jp.professional@outlook.com>
2024-05-16 21:40:06 -0300

    chore: temp fix errors

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Inter } from "next/font/google";
 import Store from "./Store";
 import { useState } from "react";
@@ -16,7 +17,7 @@ export default function HomePage() {
         <div className="flex flex-col items-center">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
             {/* Ball Section - Full width on first row */}
-            <div className="bg-green-200 p-6 rounded-lg md:col-span-2">
+            <div className="bg-green-300 p-6 rounded-lg md:col-span-2">
               <h2 className="text-2xl font-bold mb-2">Ball</h2>
               <p className="mb-4">Top-notch balls ideal for champions</p>
               <img
@@ -26,7 +27,7 @@ export default function HomePage() {
               />
             </div>
             {/* Net Section */}
-            <div className="bg-green-200 p-6 rounded-lg">
+            <div className="bg-green-300 p-6 rounded-lg">
               <h2 className="text-2xl font-bold mb-2">Net</h2>
               <p className="mb-4">Top range nets</p>
               <img
@@ -36,7 +37,7 @@ export default function HomePage() {
               />
             </div>
             {/* Shoes Section */}
-            <div className="bg-green-200 p-6 rounded-lg">
+            <div className="bg-green-300 p-6 rounded-lg">
               <h2 className="text-2xl font-bold mb-2">Shoes</h2>
               <p className="mb-4">Step into winners shoes</p>
               <img
@@ -46,10 +47,10 @@ export default function HomePage() {
               />
             </div>
             {/* Kits Section - Full width on last row */}
-            <div className="bg-green-200 p-6 rounded-lg md:col-span-2">
+            <div className="bg-green-300 p-6 rounded-lg md:col-span-2">
               <h2 className="text-2xl font-bold mb-2">Kits</h2>
               <p className="mb-4">
-                Exceptional kits of every champion's dreams
+                Exceptional kits of every champion{"'s"} dreams
               </p>
               <img
                 src="/images/c-thumb-1.jpg"

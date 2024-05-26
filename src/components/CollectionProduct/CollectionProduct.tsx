63f4d84fa60d5e4bbcc7e0239d2d156861ef652c
P <jp.professional@outlook.com>
2024-05-26 15:26:28 -0300

    feat: add external link on click on product card

--- src/components/CollectionProduct/CollectionProduct.tsx
+++ src/components/CollectionProduct/CollectionProduct.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 
-interface CollectionProductProps {
+interface CollectionProductProps extends React.HTMLAttributes<HTMLDivElement> {
   image: string;
   title: string;
   price: number;
@@ -14,19 +15,21 @@ export default function CollectionProduct({
   url,
 }: CollectionProductProps) {
   return (
-    <div className="bg-white w-[300px] lg:w-[280px] cursor-pointer rounded-lg">
-      <div className="relative group bg-white !w-full !aspect-[1/1] !max-w-full rounded-lg">
-        <Image className="rounded-lg" src={image} alt="Coleção 1" fill />
+    <Link href={url} target="_blank">
+      <div className="bg-white w-[300px] lg:w-[280px] cursor-pointer rounded-lg">
+        <div className="relative group bg-white !w-full !aspect-[1/1] !max-w-full rounded-lg">
+          <Image className="rounded-lg" src={image} alt="Coleção 1" fill />
+        </div>
+        <div className="p-8">
+          <span className="truncate block w-full">{title}</span>
+          <span className="text-gray-500 text-md ml-2">
+            {Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(price)}
+          </span>
+        </div>
       </div>
-      <div className="p-8">
-        <span className="truncate block w-full">{title}</span>
-        <span className="text-gray-500 text-md ml-2">
-          {Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(price)}
-        </span>
-      </div>
-    </div>
+    </Link>
   );
 }
